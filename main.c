37f67baf920070412b2d9731d7d425d8e07d6cb3
Xuan Hung Dao <luyendh@nal.vn>
2017-08-31 20:27:06 +0700

    Thêm hàm trừ hai số

--- main.c
+++ main.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
 
 main() {
-    printf("%i", plus(6, 7));
+    printf("%i \n", plus(6, 7));
+    printf("%i", minus(6, 7));
 }
 
 // Cộng hai số với tham số đầu vào
 // là a, b là hai số nguyên.
 int plus(int a, int b) {
     return a + b;
+}
+
+int minus(int a, int b){
+    return a - b;
 }
